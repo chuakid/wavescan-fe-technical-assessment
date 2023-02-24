7d4bee67397de028fc847ede04505dd0d7adae2c
chuakid <chuakid@gmail.com>
2023-02-24 14:06:45 +0800

    add loading screen

--- src/components/scanners/ScannersComponent.tsx
+++ src/components/scanners/ScannersComponent.tsx
@@ -1,4 +1,4 @@
-import { Box, Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr } from '@chakra-ui/react'
+import { Box, Spinner, Table, TableCaption, TableContainer, Tbody, Th, Thead, Tr } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import { successPage } from '../../api'
 import ScannerComponent from './ScannerComponent'
@@ -6,11 +6,15 @@ import ScannerDetails from './ScannerDetails'
 
 const ScannersComponent = () => {
     const [scanners, setScanners] = useState<ScannerDetails[]>()
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         successPage().then(
             (res) => {
                 res.json()
-                    .then(scanners => setScanners(scanners))
+                    .then(scanners => {                        
+                        setScanners(scanners)
+                        setLoading(false)
+                    })
                     .catch(() => console.log("Malformed scanners"))
             }
         ).catch((reason) => console.log("Failed to get scanners:" + reason))
@@ -18,24 +22,27 @@ const ScannersComponent = () => {
 
     return (
         <Box boxShadow="md" p="6" border="1px" borderColor="gray.200" borderRadius={10}>
-            <TableContainer>
-                <Table>
-                    <TableCaption>Scanners</TableCaption>
-                    <Thead>
-                        <Tr>
-                            <Th>Scanner Name</Th>
-                            <Th>IP Address</Th>
-                            <Th>Scanner Speed</Th>
-                            <Th>Status</Th>
-                        </Tr>
-                    </Thead>
+            {loading ?
+                <Spinner />
+                : <TableContainer>
+                    <Table>
+                        <TableCaption>Scanners</TableCaption>
+                        <Thead>
+                            <Tr>
+                                <Th>Scanner Name</Th>
+                                <Th>IP Address</Th>
+                                <Th>Scanner Speed</Th>
+                                <Th>Status</Th>
+                            </Tr>
+                        </Thead>
 
-                    <Tbody>
-                        {scanners?.map((scanner, i) => <ScannerComponent key={i} scanner={scanner} />)}
-                    </Tbody>
-                </Table>
+                        <Tbody>
+                            {scanners?.map((scanner, i) => <ScannerComponent key={i} scanner={scanner} />)}
+                        </Tbody>
+                    </Table>
 
-            </TableContainer>
+                </TableContainer>}
+
         </Box>
     )
 }
